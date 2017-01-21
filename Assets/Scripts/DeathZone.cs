ca15804260056b507c800485162216fc7b3a8ea5
Abhinav Rastogi <abhinav.rastogi@flipkart.com>
2017-01-21 16:02:02 +0530

    Adds gameover state

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathZone : MonoBehaviour {
+	public Canvas canvas;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Destroy(other.transform.FindChild("pug").gameObject);
+		Time.timeScale = 0;
+		canvas.gameObject.SetActive (true);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,21 +8,26 @@ public class PlayerController : MonoBehaviour {
 	public float gravity = 10.0f;
 	private Vector3 moveDirection = Vector3.zero;
 	public GameObject pug;
+	public Canvas canvas;
+	public GameObject deathZone;
 
 	void Start () {
 		controller = GetComponent<CharacterController>();
 	}
 	
 	void Update () {
-		
 		if (controller.isGrounded) {
 			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 			moveDirection *= speed;
-			pug.transform.forward = Vector3.Lerp(pug.transform.forward, moveDirection.normalized, Time.deltaTime);
+			if (moveDirection != Vector3.zero) {
+				pug.transform.forward = moveDirection;
+			}
+
 			if (Input.GetButton("Jump"))
 				moveDirection.y = jumpSpeed;
 
 		}
+
 		moveDirection.y -= gravity * Time.deltaTime;
 		controller.Move(moveDirection * Time.deltaTime);
 	}
