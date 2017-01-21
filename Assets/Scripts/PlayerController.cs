838deae5bf28d1f1380d0f47f54c314133085a66
Abhinav Rastogi <abhinav.rastogi@flipkart.com>
2017-01-21 15:13:28 +0530

    Initial commit. Day 01

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	CharacterController controller;
+	public float speed = 6.0F;
+	public float jumpSpeed = 8.0F;
+	public float gravity = 10.0f;
+	private Vector3 moveDirection = Vector3.zero;
+	public GameObject pug;
+
+	void Start () {
+		controller = GetComponent<CharacterController>();
+	}
+	
+	void Update () {
+		
+		if (controller.isGrounded) {
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection *= speed;
+			pug.transform.forward = Vector3.Lerp(pug.transform.forward, moveDirection.normalized, Time.deltaTime);
+			if (Input.GetButton("Jump"))
+				moveDirection.y = jumpSpeed;
+
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+}
