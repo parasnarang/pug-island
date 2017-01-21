77eda4fe49d27fa0b8dce6a1697a00304acd386f
Abhinav Rastogi <abhinav.rastogi@flipkart.com>
2017-01-21 21:07:27 +0530

    Adds cross platform input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,72 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerController : MonoBehaviour {
 //	CharacterController controller;
 	public float speed = 6.0F;
 	public float jumpSpeed = 8.0F;
-	public float gravity = 10.0f;
+//	public float gravity = 10.0f;
 	private Vector3 moveDirection = Vector3.zero;
 	public GameObject pug;
 	public Canvas canvas;
 	public GameObject deathZone;
 
-	Collider coll;
-//	private Rigidbody playerRigidBody;
+//	Collider coll;
+	private Rigidbody playerRigidBody;
 	float distToGround;
+
 	void Start () {
 //		controller = GetComponent<CharacterController>();
-//		playerRigidBody = gameObject.GetComponent<Rigidbody>();
-		coll = gameObject.GetComponent<Collider>();
-		distToGround = coll.bounds.extents.y;
-		Debug.Log (distToGround);
+		playerRigidBody = gameObject.GetComponent<Rigidbody>();
+//		coll = gameObject.GetComponent<Collider>();
 	}
-	
-//	void Update () {
-//		if (controller.isGrounded) {
-//			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-//			moveDirection *= speed;
-////			if (moveDirection != Vector3.zero) {
-////				pug.transform.forward = moveDirection;
-////			}
-//
-//			RaycastHit hit = new RaycastHit();
-//
-//
-//			if (Input.GetButton("Jump"))
-//				moveDirection.y = jumpSpeed;
-//
-//			if (Input.GetMouseButtonDown(0)) {
-////				Debug.Log ("click");
-////				Debug.Log (Input.mousePosition);
-//				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-////				Debug.DrawRay (transform.position, ray.direction * 20, Color.red, 4.0f);
-//				pug.transform.forward = ray.direction;
-//				if(Physics.Raycast(ray, out hit, 20.0f)) {
-//					Debug.Log(hit.collider.transform);
-//				}
-//			}
-//
-//		}
-//
-//		moveDirection.y -= gravity * Time.deltaTime;
-//		controller.Move(moveDirection * Time.deltaTime);
-//	}
 
 	bool isGrounded() {
-		bool hit = Physics.Raycast (transform.position, -Vector3.up, distToGround + 0.1f);
-		Debug.Log (hit);
+		bool hit = Physics.Raycast (transform.position + new Vector3(0, 0.1f, 0), Vector3.down, 0.1f);
 		return hit;
 	}
 
 	void FixedUpdate() {
-		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-		moveDirection *= speed;
+		moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+		moveDirection = moveDirection.normalized * speed;
 		if (Input.GetButton ("Jump") && isGrounded()) {
-			moveDirection.y = jumpSpeed;
+			//moveDirection.y = jumpSpeed;
+			playerRigidBody.velocity = new Vector3(0, jumpSpeed * Time.deltaTime, 0);
+//			playerRigidBody.velocity.y = jumpSpeed * Time.deltaTime;
 
 		}
-		transform.position += moveDirection * Time.deltaTime;
+//		transform.position += moveDirection * Time.deltaTime;
+		playerRigidBody.MovePosition(transform.position + moveDirection * Time.deltaTime);
 
 		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
 		RaycastHit floorHit;
