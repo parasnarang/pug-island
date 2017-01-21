78a6e0aa5502ecaa10b1fecc4851303e5d7f3951
Paras Narang <narangparas@gmail.com>
2017-01-21 17:08:44 +0530

    Added earthquake animation for island

---
+++ Assets/Scripts/IslandEarthquakeScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IslandEarthquakeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
